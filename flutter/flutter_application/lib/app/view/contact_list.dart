b1369ae8bf0c454a1d5052986be1e36d3f54d87f
Gu3001 <gustavo180g@gmail.com>
2021-03-21 16:07:36 -0300

    feito até aula 14, lista implementada

--- flutter/flutter_application/lib/app/view/contact_list.dart
+++ flutter/flutter_application/lib/app/view/contact_list.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import '../my_app.dart';
 
 class ContactList extends StatelessWidget {
+  var lista = [
+    {'nome':'Deku','telefone':'(44)91111-1111', 'avatar':'https://media.tenor.com/images/bd54bef392a04239c6b46a976a473a43/tenor.gif'},
+    {'nome':'All-might','telefone':'(44)92222-2222', 'avatar':'https://giffiles.alphacoders.com/103/103159.gif'},
+    {'nome':'Uraraka','telefone':'(44)933333-3333', 'avatar':'https://66.media.tumblr.com/8685b856b3495a119bf4913e4783a093/tumblr_o5s0bf6laz1qa94xto2_500.gif'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     //define componentes por padrão
@@ -18,7 +24,28 @@ class ContactList extends StatelessWidget {
             )
         ],
         ),
-      body: ListView(),
+      body: ListView.builder(
+        itemCount: lista.length,
+        itemBuilder: (context, i){
+          var contato = lista[i];
+          var avatar = CircleAvatar(backgroundImage: NetworkImage(contato['avatar']),);
+          return ListTile(
+            leading: avatar,
+            title: Text(contato['nome']),
+            subtitle: Text(contato['telefone']),
+            trailing: Container(
+              width: 100,
+              child:Row(
+                children: [
+                  IconButton(icon: Icon(Icons.edit), onPressed: null),
+                  IconButton(icon: Icon(Icons.delete), onPressed: null),
+
+                ],
+              )
+              ),
+          );
+        },
+      ),
     );
   }
 }
